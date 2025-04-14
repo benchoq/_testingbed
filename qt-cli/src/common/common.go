dce9dbad6a743ceceea2eb6459d953a0de1892f7
Ben Cho <ben.cho@qt.io>
2025-04-14 17:28:53 +0200

    remove unused variables, code

--- qt-cli/src/common/common.go
+++ qt-cli/src/common/common.go
@@ -3,28 +3,6 @@
 
 package common
 
-import (
-	"fmt"
-
-	"github.com/charmbracelet/lipgloss"
-)
-
-const QtCliName = "Qt CLI"
-const QtCliExec = "qtcli"
-const QtCliVersion = "0.1"
-
-var QtCliInfoString string
-var QtCliInfoDecorated string
-
 const PromptFileName = "prompt.yml"
 const TemplateFileName = "templates.yml"
 const UserPresetFileName = ".qtcli.preset"
-
-func init() {
-	QtCliInfoString = fmt.Sprintf("%s v%s", QtCliName, QtCliVersion)
-	QtCliInfoDecorated = lipgloss.
-		NewStyle().
-		Foreground(lipgloss.Color("#5a33f7")).
-		Render(QtCliInfoString)
-
-}

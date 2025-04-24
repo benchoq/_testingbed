76cfec6181f76cffeb3eece5c1c1333b168fcfcd
Ben Cho <ben.cho@qt.io>
2025-04-24 18:53:06 +0200

    add preset description on input page

--- qt-core/webview-ui/src/app/PageParamInput.svelte
+++ qt-core/webview-ui/src/app/PageParamInput.svelte
@@ -4,8 +4,7 @@ SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
 -->
 
 <script lang="ts">
-  import { Checkbox, Button } from "flowbite-svelte";
-  import { ArrowRightOutline } from 'flowbite-svelte-icons';
+  import { Checkbox, Label } from "flowbite-svelte";
 
   import SectionLabel from "@/comps/SectionLabel.svelte";
   import NameInput from "./NameInput.svelte";
@@ -18,21 +17,26 @@ SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
 
 <div class={`w-full h-full grid
   grid-cols-[max-content_1fr]
-  grid-rows-[repeat(2,min-content)_1fr_repeat(3,min-content)]
+  grid-rows-[min-content_1fr_repeat(3,min-content)]
   gap-2`}
 >
-
-  <SectionLabel text="You're creating a new project" class="col-span-2" />
   <SectionLabel text="Preset" />
   <div>&quot;{createPresetDisplayText(presets.selected)}&quot;</div>
 
   <div></div>
+  <div>
+    <Label class="qt-label whitespace-pre-wrap leading-relaxed"
+      >{(presets.selected?.meta?.description ?? "").replaceAll("\n", "\n\n")}
+    </Label>
+  </div>
+
+  <SectionLabel text="Options" class="h-full mt-3" />
   <div class="w-1/2 flex flex-col gap-2">
     <PresetOptionsTable /> 
   </div>
 
   <SectionLabel text="Name" /><NameInput />
-  <SectionLabel text="Create In" class="h-full"/>
+  <SectionLabel text="Create In" class="h-full mt-3"/>
   <div>
     <WorkingDirInput />
     {#if configs.type === "project"}
@@ -40,12 +44,10 @@ SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
         class="self-start qt-checkbox mt-2"
         bind:checked={configs.saveWorkingDir}
       >
-        Use as a default project directory
+        Use as default project directory
       </Checkbox>
     {:else}
       <div></div>
     {/if}
   </div>
-
-  <div class="col-span-2"></div>
 </div>

7da87975e80d902b0c58b0148386e22ac46bda51
Ben Cho <ben.cho@qt.io>
2025-06-19 10:42:53 +0200

    make onMount impl. of app to be async one. update disabled button border's default value

--- qt-core/webview-ui/src/app/App.svelte
+++ qt-core/webview-ui/src/app/App.svelte
@@ -14,7 +14,7 @@ SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
   let container: HTMLDivElement;
 
   onMount(() => { 
-    onAppMount();
+    void onAppMount();
     requestAnimationFrame(() => {
       focusAnyChild(container);
     });
